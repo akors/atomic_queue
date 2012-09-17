412c8dd71e8eba657f87f445dfe2dccc72ce7d78
Alexander Korsunsky <fat.lobyte9@gmail.com>
2012-09-17 14:13:11 +0200

    Add Doxygen-style documentation

--- atomic_queue.hpp
+++ atomic_queue.hpp
@@ -17,26 +17,48 @@ namespace aq {
 
 namespace detail {
 
+/** A node in the linked list. */
 template <typename T>
 struct node
 {
-    T t;
-    std::atomic<node<T>*> next;
+    T t /**< Value */; 
+    std::atomic<node<T>*> next /**< Next node in list */; 
 };
 
 
+/** A thread-safe and lock-free queue container.
+*
+* Use this class as a queue where multiple threads can read from and write
+* to at the same time.
+* To add elements to the queue, use the push() function.
+* To remove and retrieve elements, use the pop() function, but remember to 
+* deallocate the value with deallocate() after using it.
+*
+* @tparam T Type of the objects this queue will hold.
+* @tparam Allocator Allocator type
+*/
 template <typename T, typename Allocator = std::allocator<T> >
 class atomic_queue
 {
 public:
+
+    /** Construct an empty qeue.
+    *
+    * @param alc Allocator object that is to be used for memory
+    * allocation/deallocation. 
+    *
+    * @note The queue is thread-safe after this function has returned.
+    */
     atomic_queue(const Allocator& alc = Allocator()) noexcept
         : size_(0u), front_(nullptr), end_(nullptr),
         alc_(alc)
-    {
-    }
+    { }
 
-    // NOT threadsafe!!! Why would you call the destructor if the object is in
-    // use? Are you crazy?
+
+    /** Destructor.
+    * 
+    * @note The queue is thread-safe before this function is invoked.
+    */
     ~atomic_queue() noexcept
     {
         node<T>* fr = front_; // atomic_load
@@ -51,6 +73,14 @@ public:
 
     }
 
+    /** Push object into the queue.
+    *
+    * @param t Object you want to push into the queue. Requires T to be
+    * CopyConstructible.
+    * @throws Any exceptions thrown by the copy constructor of the object.
+    *
+    * @note This function is Thread-safe, lock-free and wait-free.
+    */
     void push(const T& t)
     {
         typename NodeAllocator::pointer new_node = alc_.allocate(1);
@@ -67,6 +97,18 @@ public:
         push_node(new_node);
     }
 
+    /** Pop object from the queue.
+    *
+    * This function removes an object from the front of the queue and returns
+    * a pointer to it. Do not delete the object manually!
+    *
+    * To delete an object returned by this function, use the deallocate()
+    * member function.
+    *
+    * @return A pointer to the object that was removed from the queue.
+    *
+    * @note This function is Thread-safe, lock-free but not wait-free.
+    */
     T* pop() noexcept
     {
         node<T>* old_front = front_;
@@ -87,6 +129,16 @@ public:
         return reinterpret_cast<T*>(old_front);
     }
 
+    /** Deallocate an object returned by pop().
+    *
+    * This function must be used to destroy objects that are returned by pop().
+    * Do not use this function with an object that was not returned by pop(),
+    * even if the type matches.
+    *
+    * @param obj A pointer to an object returned by pop().
+    *
+    * @note This function is Thread-safe, lock-free but not wait-free.
+    */
     void deallocate(T* obj) noexcept
     {
         if (!obj) return;
@@ -96,18 +148,31 @@ public:
 
         // nodes with next == 0 are still referenced by an executing
         // push() function and the next ptr will be modified.
-        // Since we don't want the function to write to deallocated
-        // memory, we hang in a loop until the node has a non-zero next ptr.
+        // Since we don't want push() to write to deallocated memory, we hang
+        // in a loop until the node has a non-zero next ptr.
         while(!reinterpret_cast<node<T>*>(obj)->next.load())
             std::this_thread::yield();
 
         alc_.deallocate(reinterpret_cast<node<T>*>(obj), 1);
     }
 
+
+    /** Get size of the queue.
+    *
+    * This function returns an approximation of the current queue size.
+    * In a multithreaded environment, this value might change immediately
+    * before or after the invocation. Especially, it is *not* guaranteed that
+    * if(size() != 0) assert(pop() != nullptr) !
+    *
+    * In multithreaded environments, please consider to not use this function.
+    *
+    * @return Current size of the queue.
+    *
+    * @note This function is Thread-safe, lock-free and wait-free.
+    */
     std::size_t size() const noexcept
-    {
-        return size_;
-    }
+    { return size_; }
+
 protected:
 
     void push_node(node<T>* new_node) noexcept
@@ -128,10 +193,10 @@ protected:
     typedef Allocator ValueAllocator;
     typedef typename Allocator::template rebind<node<T> >::other NodeAllocator;
 
-    std::atomic_size_t size_;
-    std::atomic<node<T>*> front_;
-    std::atomic<node<T>*> end_;
-    NodeAllocator alc_;
+    std::atomic_size_t size_ /**< Current size of queue. Not reliable. */;
+    std::atomic<node<T>*> front_ /**< Front of the queue. */;
+    std::atomic<node<T>*> end_ /**< End of the queue. */;
+    NodeAllocator alc_ /**< Allocator for node<T> objects. */;
 };
 
 } // namespace detail
